9ab231277da6b2bcd565fa403e6641b4416a30b7
Will Leinweber <will@bitfission.com>
2009-05-27 21:52:54 -0500

    get about page and css up

--- config/routes.rb
+++ config/routes.rb
@@ -34,10 +34,11 @@ ActionController::Routing::Routes.draw do |map|
   # map.root :controller => "welcome"
 
   # See how all your routes lay out with "rake routes"
+  map.root :controller => "runs", :action => "new"
 
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  map.connect ':name', :controller => 'page', :action => 'show'
+
 end
